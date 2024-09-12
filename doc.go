607ab69759a8535d04e55c792648d039b300f022
Aleksandr Kramarenko <akramarenkov@yandex.ru>
2024-09-12 05:54:00 +0300

    desc improved

---
+++ doc.go
@@ -0,0 +1,2 @@
+// Library that allows you to divide a sequence of something into spans.
+package span
--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package span
 
 import "golang.org/x/exp/constraints"
 
+// Describes span.
 type Span[Type constraints.Ordered] struct {
 	Begin Type
 	End   Type
